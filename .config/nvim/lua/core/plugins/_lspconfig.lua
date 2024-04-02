5965d16db614f24687abf7b4494892ee89cf19f8
James Baldwin <jwbaldwin3@aol.com>
2024-04-02 15:41:57 -0400

    personal

--- .config/nvim/lua/core/plugins/_lspconfig.lua
+++ .config/nvim/lua/core/plugins/_lspconfig.lua
@@ -2,6 +2,7 @@ local present, lspconfig = pcall(require, "lspconfig")
 if not present then
   return
 end
+local configs = require("lspconfig.configs")
 
 local utils = require("core.utils")
 
@@ -70,26 +71,49 @@ lspconfig.lua_ls.setup {
   },
 }
 
-local elixirls = ""
-if os.getenv("USER") == "jbaldwin" then
-  elixirls = "/Users/jbaldwin/.local/share/nvim/mason/packages/elixir-ls/language_server.sh"
-else
-  -- Mason's install location
-  elixirls = "/Users/jwbaldwin/.local/share/nvim/mason/packages/elixir-ls/language_server.sh"
-end
 
-lspconfig.elixirls.setup {
-  cmd = { elixirls },
-  on_attach = default_on_attach,
-  capabilities = capabilities,
-  filetypes = { "elixir", "eelixir", "heex", "surface", "eex" },
-  settings = {
-    elixirLS = {
-      dialyzerEnabled = false,
-      fetchDeps = true,
-    },
-  },
-}
+-- local lexical_config = {
+--   filetypes = { "elixir", "eelixir", "heex", "eex" },
+--   cmd = { "/Users/jbaldwin/repos/lexical/_build/dev/package/lexical/bin/start_lexical.sh" },
+--   settings = {},
+-- }
+--
+-- if not configs.lexical then
+--   configs.lexical = {
+--     default_config = {
+--       filetypes = lexical_config.filetypes,
+--       cmd = lexical_config.cmd,
+--       root_dir = function(fname)
+--         return lspconfig.util.root_pattern("mix.exs", ".git")(fname) or vim.loop.os_homedir()
+--       end,
+--       -- optional settings
+--       settings = lexical_config.settings,
+--     },
+--   }
+-- end
+
+-- lspconfig.lexical.setup({})
+
+-- local elixirls = ""
+-- if os.getenv("user") == "jbaldwin" then
+--   elixirls = "/users/jbaldwin/.local/share/nvim/mason/packages/elixir-ls/language_server.sh"
+-- else
+--   -- mason's install location
+--   elixirls = "/users/jwbaldwin/.local/share/nvim/mason/packages/elixir-ls/language_server.sh"
+-- end
+--
+-- lspconfig.elixirls.setup {
+--   cmd = { elixirls },
+--   on_attach = default_on_attach,
+--   capabilities = capabilities,
+--   filetypes = { "elixir", "eelixir", "heex", "surface", "eex" },
+--   settings = {
+--     elixirLS = {
+--       dialyzerEnabled = false,
+--       fetchDeps = true,
+--     },
+--   },
+-- }
 
 lspconfig.tailwindcss.setup {
   on_attach = default_on_attach,
